3c662e1e4e907441715b28ab58d088b6ccaef72f
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2025-03-24 09:18:30 -0300

    chore: changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,51 +1,53 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+opt.relativenumber = false
+opt.wrap = true
+
 vim.fn.sign_define("DapBreakpoint", {
-    text = "",
-    texthl = "DiagnosticSignError",
-    linehl = "",
-    numhl = ""
+  text = "",
+  texthl = "DiagnosticSignError",
+  linehl = "",
+  numhl = "",
 })
 
 vim.fn.sign_define("DapBreakpointRejected", {
-    text = "",
-    texthl = "DiagnosticSignError",
-    linehl = "",
-    numhl = ""
+  text = "",
+  texthl = "DiagnosticSignError",
+  linehl = "",
+  numhl = "",
 })
 vim.fn.sign_define("DapStopped", {
-    text = "",
-    texthl = "DiagnosticSignWarn",
-    linehl = "Visual",
-    numhl = "DiagnosticSignWarn"
+  text = "",
+  texthl = "DiagnosticSignWarn",
+  linehl = "Visual",
+  numhl = "DiagnosticSignWarn",
 })
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
-    callback = function()
-        local buf_ft = vim.bo.filetype
+  callback = function()
+    local buf_ft = vim.bo.filetype
 
-        if buf_ft == "dapui_breakpoints" then
-            vim.cmd("setlocal winbar=\\ Breakpoints")
-        end
-        if buf_ft == "dapui_stacks" then
-            vim.cmd("setlocal winbar=\\\\ Call\\ Stack")
-        end
-
-        if buf_ft == "dapui_scopes" then
-            vim.cmd("setlocal winbar=\\\\ Variables")
-        end
-
-        if buf_ft == "dapui_watches" then
-            vim.cmd("setlocal winbar=\\󰂥\\ Watches")
-        end
-        if buf_ft == "dapui_console" then
-            vim.cmd("setlocal winbar=\\ Console")
-        end
-        if buf_ft == "dap-repl" then
-            vim.cmd("setlocal winbar=\\ REPL")
-        end
-
+    if buf_ft == "dapui_breakpoints" then
+      vim.cmd("setlocal winbar=\\ Breakpoints")
+    end
+    if buf_ft == "dapui_stacks" then
+      vim.cmd("setlocal winbar=\\\\ Call\\ Stack")
     end
 
+    if buf_ft == "dapui_scopes" then
+      vim.cmd("setlocal winbar=\\\\ Variables")
+    end
+
+    if buf_ft == "dapui_watches" then
+      vim.cmd("setlocal winbar=\\󰂥\\ Watches")
+    end
+    if buf_ft == "dapui_console" then
+      vim.cmd("setlocal winbar=\\ Console")
+    end
+    if buf_ft == "dap-repl" then
+      vim.cmd("setlocal winbar=\\ REPL")
+    end
+  end,
 })
